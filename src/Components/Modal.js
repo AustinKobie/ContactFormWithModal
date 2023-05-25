a05a90dd4328182d98c45771e8adde734e16ac71
AustinKobie <110858945+AustinKobie@users.noreply.github.com>
2023-05-25 02:30:23 -0400

    first commit

---
+++ src/Components/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Modal.css";
+
+function Modal({ closeModal, userName }) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="modalClose">
+          <button onClick={closeModal}>X</button>
+        </div>
+        <div className="title">
+          <h1>Modal Title</h1>
+        </div>
+        <div className="body">
+          <p>Thank you, {userName}, for getting in contact with us!</p>
+        </div>
+        <div className="footer">
+          <button  className="close-btn" onClick={closeModal}>Cancel</button>
+          <button className="continue-btn">Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
---
+++ src/Components/contactForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./contactForm.css";
+import Modal from "./Modal";
+
+const Form = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setOpenModal(true);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Contact us</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="email" name="email" placeholder="Email" />
+        <textarea name="message" placeholder="Message" cols="30" rows="6" />
+        <div className="checkboxContainer">
+          <input
+            className="check"
+            type="checkbox"
+            name="checkbox"
+            defaultChecked
+          /> <label htmlFor="checkbox">I agree to being sent marketing and email offers.</label>
+        </div>
+        <button
+          className="openModal"
+          type="submit"
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          send
+        </button>
+      </form>
+      {openModal && (
+        <Modal closeModal={() => setOpenModal(false)} userName={name} />
+      )}
+    </div>
+  );
+};
+
+export default Form;
